41774690815bcdef2ac21e4e3e97f37fb5700728
@Rewan-Adel <rewanadel1266@gmail.com>
2024-05-20 00:45:05 +0300

    Rename LikedUsers attribute into likedBy

--- Modules/Review/models/Review.js
+++ Modules/Review/models/Review.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-    residenceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Residence' },
-    userId     : { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
-    rating     : { type: Number, required: true },
-    comment    : { type: String },
-    likes      : { type: Number, default: 0 },
-    likedUsers  : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }]
+    residenceId : { type: mongoose.Schema.Types.ObjectId, ref: 'Residence' },
+    userId      : { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
+    rating      : { type: Number, required: true },
+    comment     : { type: String },
+    reviewLikes : { type: Number, default: 0 },
+    likedBy     : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }]
 },{
     timestamps: true
 });
