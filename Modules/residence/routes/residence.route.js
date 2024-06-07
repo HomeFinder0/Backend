aad6768f703b86eea397729937020eb5d3cd63f4
@Rewan-Adel <rewanadel1266@gmail.com>
2024-06-07 14:41:19 +0300

    adding the routes

--- Modules/residence/routes/residence.route.js
+++ Modules/residence/routes/residence.route.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
-const {
+const {     
     createResidence,
-    completeResidence,
-    stepTwoComplete,
-    stepThreeComplete,
-    stepFourComplete,
-    
+    stepOneUpdate,
+    stepTwoUpdate,
+    stepThreeUpdate,
+    stepFourUpdate,
+    uploadResidenceImages,
     setLocation,
-    residenceImages,
+    updateResidence,
+
+    deleteResidenceImage,
+    getResidenceImages,
+    getLocation,
     
     getOneResidence,
     getApproved,
@@ -16,36 +20,39 @@ const {
     getAllApproved,
     getNearestResidences,
     
-    updateResidence,
     deleteOneResidence,
-    deleteResidenceImage,
-    filtration,
-} = require("../controllers/residence.controller.js");
-const authMiddleware = require("../../authentication/middlewares/auth.middleware.js");
+    filtration
+                }      = require("../controllers/residence.controller.js");
+const authMiddleware   = require("../../authentication/middlewares/auth.middleware.js");
 const {uploadMultiple} = require("../../../Utils/multer.js");
 
 router.use(authMiddleware);
 
 router.post("/create", createResidence);
-router.post("/complete/1st/:residenceId" , completeResidence);
-router.post("/complete/2nd/:residenceId" , stepTwoComplete);
-router.post("/complete/3rd/:residenceId" , stepThreeComplete);
-router.post("/complete/4th/:residenceId" , stepFourComplete);
+router.post("/complete/1st/:residenceId" , stepOneUpdate);
+router.post("/complete/2nd/:residenceId" , stepTwoUpdate);
+router.post("/complete/3rd/:residenceId" , stepThreeUpdate);
+router.post("/complete/4th/:residenceId" , stepFourUpdate);
+router.post("/location/:residenceId" ,     setLocation);
+router.post("/upload/:residenceId", uploadMultiple , uploadResidenceImages);
 
-router.post("/location/:residenceId" , setLocation);
-router.post("/upload/:residenceId", uploadMultiple , residenceImages);
+router.get("/get/:residenceId",       getOneResidence);
+router.get("/pending?",               getPending);
+router.get("/approved?",              getApproved);
+router.get("/sold?",                  getSold);
+router.get("/nearest?",               getNearestResidences);
+router.get("/all?",                   getAllApproved);
+router.get("/images/:residenceId",    getResidenceImages );
+router.get("/location/:residenceId",  getLocation);
+router.get("/filter",                 filtration);
 
-router.get("/get/:residenceId",  getOneResidence);
-router.get("/pending?",          getPending);
-router.get("/approved?",         getApproved);
-router.get("/sold?",             getSold);
-router.get("/nearest?",          getNearestResidences);
-router.get("/all?",              getAllApproved);
+router.patch("/update/:residenceId"    , updateResidence);
+router.patch("/update/1st/:residenceId" , stepOneUpdate);
+router.patch("/update/2nd/:residenceId" , stepTwoUpdate);
+router.patch("/update/3rd/:residenceId" , stepThreeUpdate);
+router.patch("/update/4th/:residenceId" , stepFourUpdate);
 
-router.patch("/update/:residenceId", updateResidence);
 router.delete("/delete/:residenceId", deleteOneResidence);
 router.delete("/delete/images/:residenceId", deleteResidenceImage);
-
-router.get("/filter", filtration);
 
 module.exports = router;
