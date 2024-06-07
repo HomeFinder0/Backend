e8b981ad8e062cb5c3c1072164bea137da105c26
@Rewan-Adel <rewanadel1266@gmail.com>
2024-06-07 14:33:00 +0300

    Convert response not only request for ML

--- Helpers/converter.js
+++ Helpers/converter.js
@@ -1,16 +1,27 @@
+exports.utilitiesConverter = (value) => {
+    const conversion = {
+        'AllPub' : 'All public Utilities',
+        'ELO'    : 'Electricity',
+        'NoSeWa' : 'Electricity and Gas',
+        'NoSewr' : 'Electricity and Water'
+    };
+    return conversions[value];
+}
+
+
 exports.msSubClassConverter = (value) => {
     const conversions = {
-        "1story and newer" : 20,
-        "1story and older" : 30,
-        "2story and newer" : 60,
-        "2story and older" : 70,
-        "1 story finished attic" : 40,
-        "1 story unfinished attic" : 45,
-        "multi-level" : 80,
-        "split foyer" : 85,
-        "duplex" : 90,
-        "multilevel and split" : 180,
-        "2 family conversion" : 190,
+        "1story and newer" : 20,                20 : "1story and newer",
+        "1story and older" : 30,                30 : "1story and older",
+        "2story and newer" : 60,                60 : "2story and newer",
+        "2story and older" : 70,                70 : "2story and older",
+        "1 story finished attic" : 40,          40 : "1 story finished attic",
+        "1 story unfinished attic" : 45,        45 : "1 story unfinished attic",
+        "multi-level" : 80,                     80 : "multi-level",
+        "split foyer" : 85,                     85 : "split foyer",
+        "duplex" : 90,                          90 : "duplex",
+        "multilevel and split" : 180,           180 : "multilevel and split",
+        "2 family conversion" : 190,            190 : "2 family conversion"
     };
 
     return conversions[value];
@@ -18,11 +29,11 @@ exports.msSubClassConverter = (value) => {
 
 exports.bldgTypeConverter = (value) => {
     const conversions = {
-        'single-family' : '1Fam',
-        'two-family': '2fmCon',
-        'duplex': 'Duplx',
-        'townhouse end unit': 'TwnhsE',
-        'townhouse inside unit': 'TwnhsI'
+        'single-family' : '1Fam',          '1Fam': 'single-family',
+        'two-family': '2fmCon',            '2fmCon': 'two-family',
+        'duplex': 'Duplx',                 'Duplx': 'duplex',
+        'townhouse end unit':              'TwnhsE', 'TwnhsE': 'townhouse end unit',
+        'townhouse inside unit':'TwnhsI' , 'TwnhsI': 'townhouse inside unit'
     };
 
     return conversions[value];
@@ -30,12 +41,12 @@ exports.bldgTypeConverter = (value) => {
 
 exports.qualityRatingConverter = (value) => {
     const conversions = {
-        'good'         : 'Gd',
-        'fair'         : 'Fa',
-        'poor'         : 'Po',
-        'average'      : 'TA',
-        'excellent'    : 'Ex',
-        'not available': 'NA'
+        'good'         : 'Gd',   'Gd': 'good',
+        'fair'         : 'Fa',   'Fa': 'fair',
+        'poor'         : 'Po',   'Po': 'poor',
+        'average'      : 'TA',   'TA': 'average',
+        'excellent'    : 'Ex',   'Ex': 'excellent',
+        'not available': 'NA',   'NA': 'not available'
     };
 
     return conversions[value];
@@ -84,14 +95,14 @@ exports.neighborhoodConverter = (value) => {
 
 exports.mszoningConverter = (value) => {
     const conversions = {
-        'agricultural'   : 'A',
-        'commercial'     : 'C',
-        'floating village': 'FV',
-        'industrial'      : 'I',
-        'high residential': 'RH',
-        'low' : 'RL',
-        'park'        : 'RP',
-        'medium': 'RM'
+        'agricultural'     : 'A',    'A' : 'agricultural',
+        'commercial'       : 'C',    'C' : 'commercial',
+        'floating village' : 'FV',   'FV': 'floating village',
+        'industrial'       : 'I',    'I' : 'industrial',
+        'high residential' : 'RH',   'RH': 'high residential',
+        'low'              : 'RL',   'RL': 'low',
+        'park'             : 'RP',   'RP': 'park',
+        'medium'           : 'RM',   'RM': 'medium'   
     };
 
     return conversions[value];
@@ -99,43 +110,43 @@ exports.mszoningConverter = (value) => {
 
 exports.roofMaterialsConverter = (value) => {
     const conversions = {
-        'roll'             : 'Roll',
-        'metal'            : 'Metal',
-        'membran'          : 'Membran',
-        'wood shakes'      : 'WdShake',
-        'gravel & tar'     : 'Tar&Grv',
-        'clay or tile'     : 'ClyTile',
-        'wood shingles'    : 'WdShngl',
-        'standard shingle' : 'CompShg',
+        'roll'             : 'Roll',      'roll' : 'roll',
+        'metal'            : 'Metal',     'Metal': 'metal',
+        'membran'          : 'Membran',   'Membran': 'membran',
+        'wood shakes'      : 'WdShake',   'WdShake': 'wood shakes',
+        'gravel & tar'     : 'Tar&Grv',   'Tar&Grv': 'gravel & tar',
+        'clay or tile'     : 'ClyTile',   'ClyTile': 'clay or tile',
+        'wood shingles'    : 'WdShngl',   'WdShngl': 'wood shingles',
+        'standard shingle' : 'CompShg',   'CompShg': 'standard shingle'
     }
     return conversions[value];
 };
 
 exports.garageConverter = (value) => {
     const conversions = {
-        'finished'      : 'Fin',
-        'rough finished': 'RFn',
-        'unfinished'    : 'Unf',
-        'not available' : 'NA',
+        'finished'          : 'Fin',         'Fin': 'finished',
+        'rough finished'    : 'RFn',         'RFn': 'rough finished',
+        'unfinished'        : 'Unf',         'Unf': 'unfinished',
+        'not available'     : 'NA',          'NA' : 'not available',
 
-        'built in'          : 'BuiltIn',
-        'car port'          : 'CarPort',
-        'more than one'     : '2Types',
-        'basement garage'   : 'Basment',
-        'attached to home'  : 'Attchd',
-        'Detached from home': 'Detchd' 
+        'built in'          : 'BuiltIn',     'BuiltIn': 'built in',
+        'car port'          : 'CarPort',     'CarPort': 'car port',
+        'more than one'     : '2Types',      '2Types' : 'more than one',
+        'basement garage'   : 'Basment',     'Basment': 'basement garage',
+        'attached'  : 'Attchd',      'Attchd' : 'attached',
+        'Detached': 'Detchd',      'Detchd' : 'Detached' 
     }
     return conversions[value];
 }
 
 exports.saleConditionConverter = (value) => {
     const conversions = {
-        'normal'                 : 'Normal',
-        'abnormal'               : 'Abnorml',
-        'adjoining land purchase': 'AdjLand',
-        'allocation'             : 'Alloca',
-        'family'                 : 'Family',
-        'partial'                : 'Partial'
+        'normal'                 : 'Normal', 'Normal': 'normal',
+        'abnormal'               : 'Abnorml', 'Abnorml': 'abnormal',
+        'adjoining land purchase': 'AdjLand', 'AdjLand': 'adjoining land purchase',
+        'allocation'             : 'Alloca',  'Alloca': 'allocation',
+        'family'                 : 'Family',  'Family': 'family',
+        'partial'                : 'Partial', 'Partial': 'partial'
 
     }
     return conversions[value];
@@ -143,55 +154,55 @@ exports.saleConditionConverter = (value) => {
 
 exports.saleTypeConverter = (value) => {
     const conversions = {
-        'other'                                    : 'Oth',
-        'conventional'                             : 'WD',
-        'cash'                                     : 'CWD',
-        'va loan'                                  : 'VWD',
-        'new'                                      : 'New',
-        'court officer deed/estate'                : 'COD',
-        'contract low interest'                    : 'ConLI',
-        'contract low down'                        : 'ConLD',
-        'contract 15% down payment regular terms'  : 'Con',
-        'contract low down payment and low intere' : 'ConLw',
+        'other'                                    : 'Oth',   'Oth': 'other',
+        'conventional'                             : 'WD',    'WD' : 'conventional',
+        'cash'                                     : 'CWD',   'CWD': 'cash',
+        'va loan'                                  : 'VWD',   'VWD': 'va loan',
+        'new'                                      : 'New',   'New': 'new',
+        'court officer deed/estate'                : 'COD',   'COD': 'court officer deed/estate',
+        'contract low interest'                    : 'ConLI',  'ConLI': 'contract low interest',
+        'contract low down'                        : 'ConLD',  'ConLD': 'contract low down',
+        'contract 15% down payment regular terms'  : 'Con',    'Con': 'contract 15% down payment regular terms',
+        'contract low down payment and low intere' : 'ConLw',  'ConLw': 'contract low down payment and low intere'
     }
     return conversions[value];
 }
 exports.alleyConverter = (value) =>{
     const conversions = {
-        'gravel' : 'Grvl',
-        'paved'  : 'Pave',
-        'no alley access':'NA' 	
+        'gravel' : 'Grvl',      'Grvl': 'gravel',
+        'paved'  : 'Pave',       'Pave': 'paved',
+        'no alley access':'NA' 	, 'NA': 'no alley access'
     };
 
     return conversions[value];
 }
 exports.streetConverter = (value) => {
     const conversions = {
-        'gravel' : 'Grvl',
-        'paved'  : 'Pave'
+        'gravel' : 'Grvl',      'Grvl': 'gravel',
+        'paved'  : 'Pave',       'Pave': 'paved',
     }
     return conversions[value];
 }
 
 exports.electricalConverter = (value) => {
     const conversions = {
-        'standard circuit breakers & romex' : 'SBrkr',
-        'Average' : 'FuseA',
-        'Fair' : 'FuseF',
-        'poor' : 'FuseP',
-        'mixed'   : 'Mix'
+        'standard circuit breakers & romex' : 'SBrkr',    'SBrkr': 'standard circuit breakers & romex',
+        'Average' : 'FuseA',                              'FuseA': 'Average',
+        'Fair' : 'FuseF',                                 'FuseF': 'Fair',
+        'poor' : 'FuseP',                                 'FuseP': 'poor',
+        'mixed'   : 'Mix',                                  'Mix': 'mixed'
     }
     return conversions[value];
 }
 
 exports.heatingConverter = (value) => {
     const conversions = {
-        'floor'      : 'Floor',
-        'gas'        : 'GasA',
-        'gas water'  : 'GasW',
-        'gravity'    : 'Grav',
-        'other water': 'OthW',
-        'wall'       : 'Wall'
+        'floor'      : 'Floor', 'Floor': 'floor',
+        'gas'        : 'GasA',   'GasA': 'gas',
+        'gas water'  : 'GasW',   'GasW': 'gas water',
+        'gravity'    : 'Grav',   'Grav': 'gravity',
+        'other water': 'OthW',   'OthW': 'other water',
+        'wall'       : 'Wall',   'Wall': 'wall'
     }
     return conversions[value];
 }
@@ -199,41 +210,31 @@ exports.heatingConverter = (value) => {
 
 exports.lotShapeConverter = (value) => {
     const conversions = {
-        'regular'   : 'Reg',
-        'slightly' : 'IR1',
-        'moderately' : 'IR2',
-        'irregular' : 'IR3'
+        'regular'   : 'Reg',  'Reg' : 'regular',
+        'slightly' : 'IR1',   'IR1' : 'slightly',
+        'moderately' : 'IR2', 'IR2'   : 'moderately',
+        'irregular' : 'IR3',  'IR3'   : 'irregular'
     }
     return conversions[value];
 }
 
 exports.landContourConverter = (value) => {
     const conversions = {
-        'level'   : 'Lvl',
-        'banked'  : 'Bnk',
-        'hillside': 'HLS',
-        'depression': 'Low'
+        'level'   : 'Lvl', 'Lvl' : 'level',
+        'banked'  : 'Bnk', 'Bnk' : 'banked',
+        'hillside': 'HLS', 'HLS' : 'hillside',
+        'depression': 'Low', 'Low': 'depression' 
     }
     return conversions[value];
 }
 
-// exports.utilitiesConverter = (value) => {
-//     const conversions = {
-//         'all'                         : 'AllPub',
-//         'electricity only'            : 'ELO',
-//         'electricity and gas only'    : 'NoSeWa',
-//         'electricity, gas and water'  : 'NoSewr',
-//     }
-//     return conversions[value];
-// };
-
 exports.lotConfigConverter = (value) => {
     const conversions = {
-        'inside'        : "Inside",
-        'corner'        : "Corner",
-        'cul de sac'    : "CulDSac",
-        'frontage on 2' : 'FR2',
-        'frontage on 3' : 'FR3',
+        'inside'        : "Inside", 'Inside': 'inside',
+        'corner'        : "Corner",  'Corner': 'corner',
+        'cul de sac'    : "CulDSac", 'CulDSac': 'cul de sac',
+        'frontage on 2' : 'FR2',     'FR2': 'frontage on 2',
+        'frontage on 3' : 'FR3',     'FR3': 'frontage on 3'
     };
 
     return conversions[value];
@@ -241,24 +242,24 @@ exports.lotConfigConverter = (value) => {
 
 exports.landSlopeConverter = (value) => {
     const conversions = {
-        'gentle' : 'Gtl',
-        'moderate': 'Mod',
-        'severe' : 'Sev'
+        'gentle' : 'Gtl', 'Gtl': 'gentle',
+        'moderate': 'Mod', 'Mod': 'moderate',
+        'severe' : 'Sev', 'Sev': 'severe'
     };
     return conversions[value];
 };
 
 exports.condConverter = (value) =>{
     const conversions = {
-        'normal'                    :'Norm',
-        'feeder'                    :'Feedr',
-        'arterial'                  :'Artery',
-        "east west"                 :'RRAe',
-        "north south"               :'RRAn',
-        "within 200 of east west"   :'RRNe',
-        "within 200 of North-South":'RRNn',
-        "positive off site feature" :'PosA',
-        "near positive"             :'PosN',
+        'normal'                    :'Norm',   'Norm': 'normal',
+        'feeder'                    :'Feedr',  'Feedr': 'feeder',
+        'arterial'                  :'Artery', 'Artery': 'arterial',
+        "east west"                 :'RRAe',   'RRAe': "east west",
+        "north south"               :'RRAn',   'RRAn' :"north south" ,
+        "within 200 of east west"   :'RRNe',   'RRNe': "within 200 of east west",
+        "within 200 of North-South" :'RRNn',   'RRNn': "within 200 of North-South",
+        "positive off site feature" :'PosA',   'PosA': 'positive off site feature',
+        "near positive"             :'PosN',   'PosN': 'near positive',
     };
     return conversions[value];
 };
@@ -266,78 +267,78 @@ exports.condConverter = (value) =>{
 
 exports.exteriorConverter = (value) => {
     const conversions = {
-        'asbestos shingles'         : 'AsbShng',
-        'asphalt shingles'          : 'AsphShn',
-        'brick face'                : 'BrkFace',
-        'brick common'              : 'BrkComm',
-        'cinder block'              : 'CBlock',
-        'cement board'              : 'CemntBd',
-        'hardboard'                 : 'HdBoard',
-        'hardboard siding'          : 'HdBoard',
-        'metal siding'              : 'MetalSd',
-        'plywood'                   : 'Plywood',
-        'precast concrete'          : 'PreCast',
-        'stucco'                    : 'Stucco',
-        'vinyl siding'              : 'VinylSd',
-        'wood siding'               : 'Wd Sdng',
-        'wood shingles'             : 'WdShing',
-        'other'                     : 'Other',
-        'imitation stucco'          : 'ImStucc',
-        'stone'                     :'Stone'	
+        'asbestos shingles'         : 'AsbShng', 'AsbShng': 'asbestos shingles',
+        'asphalt shingles'          : 'AsphShn', 'AsphShn': 'asphalt shingles',
+        'brick face'                : 'BrkFace', 'BrkFace': 'brick face',
+        'brick common'              : 'BrkComm', 'BrkComm': 'brick common',
+        'cinder block'              : 'CBlock',  'CBlock': 'cinder block',
+        'cement board'              : 'CemntBd', 'CemntBd': 'cement board',
+        'hardboard'                 : 'HdBoard',  'HdBoard': 'hardboard siding',
+        'hardboard siding'          : 'HdBoard',  'HdBoard': 'hardboard siding',
+        'metal siding'              : 'MetalSd',  'MetalSd': 'metal siding',
+        'plywood'                   : 'Plywood',  'Plywood': 'plywood siding',
+        'precast concrete'          : 'PreCast',  'PreCast': 'precast concrete',
+        'stucco'                    : 'Stucco',   'Stucco': 'stucco',
+        'vinyl siding'              : 'VinylSd',   'VinylSd': 'vinyl siding',
+        'wood siding'               : 'Wd Sdng',  'Wd Sdng': 'wood siding',
+        'wood shingles'             : 'WdShing',  'WdShing': 'wood shingles',
+        'other'                     : 'Other',    'Other': 'other',
+        'imitation stucco'          : 'ImStucc',  'ImStucc': 'imitation stucco',
+        'stone'                     :'Stone'	, 'Stone': 'stone'
     };
     return conversions[value];
 }
 
 exports.masVnrTypeConverter = (value) => {
     const conversions = {
-        'brick face'   : 'BrkFace',
-        'brick common' : 'BrkCmn',
-        'cinder block' : 'CBlock',
-        "none"         : "None",
-        "stone"	       : "Stone"
+        'brick face'   : 'BrkFace', 'BrkFace': 'brick face',
+        'brick common' : 'BrkCmn',  'BrkCmn': 'brick common',
+        'cinder block' : 'CBlock',  'CBlock': 'cinder block',
+        "none"         : "None",    'None': 'none',
+        "stone"	       : "Stone" , 'Stone': 'stone'
     } 
     return conversions[value];
 };
 
 exports.foundationConverter = (value)=>{
     const conversions = {
-        'slab'           : 'Slab',
-        "stone"	         : "Stone",
-        'wood'           : 'Wood',
-        'cinder block'   : 'CBlock',
-        'brick and tile' : 'BrkTil',
-        'poured contrete': 'PConc'
+        'slab'           : 'Slab',   'Slab': 'slab',
+        "stone"	         : "Stone",   'Stone': 'stone',
+        'wood'           : 'Wood',    'Wood': 'wood',
+        'cinder block'   : 'CBlock',  'CBlock': 'cinder block',
+        'brick and tile' : 'BrkTil',  'BrkTil': 'brick and tile',
+        'poured contrete': 'PConc',    'PConc': 'poured contrete'
     };
     return conversions[value];
 };
 
 exports.bsmtFinType1Converter = (value)=>{
     const conversions = {
-        'good'             : 'GLQ',
-        "average"	       : "ALQ",
-        'below average'    : 'BLQ',
-        'average rec room' : 'Rec',
-        'low'              : 'LwQ',
-        'unfinished'       : 'Unf',
+        'good'             : 'GLQ',  'GLQ': 'good',
+        "average"	       : "ALQ",  'ALQ': 'average',
+        'below average'    : 'BLQ',  'BLQ': 'below average',
+        'average rec room' : 'Rec',  'Rec': 'average rec room',
+        'low'              : 'LwQ',  'LwQ':  'low'   ,
+        'unfinished'       : 'Unf',  'Unf': 'unfinished',
     };
     return conversions[value];
 };
 
 exports.bsmtExposureConverter = (value) => {
     const conversions = {
-        'good'        : 'Gd',
-        'average'     : 'Av',
-        'minimum'     : 'Mn',
-        'no exposure' : 'No' ,
+        'good'        : 'Gd',   'Gd': 'good',
+        'average'     : 'Av',   'Av': 'average',
+        'minimum'     : 'Mn',   'Mn': 'minimum',
+        'no exposure' : 'No' ,  'No': 'no exposure'
     };
     return conversions[value];
 
 }
 exports.pavedDriveConverter = (value) => {
     const conversions = {
-        'paved'    : 'Y',
-        'gravel'   : 'N',
-        'partial'  : 'P'
+        'paved'    : 'Y',    'Y'    : 'paved',
+        'gravel'   : 'N',    'N'    : 'gravel',
+        'partial'  : 'P',     'P'    : 'partial',
     };
     return conversions[value];
 };
