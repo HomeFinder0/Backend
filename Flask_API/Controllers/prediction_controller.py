f5343709016a39968875981688a937cc97372167
@Rewan-Adel <rewanadel1266@gmail.com>
2024-06-13 16:58:28 +0300

    Prediction API

--- Flask_API/Controllers/prediction_controller.py
+++ Flask_API/Controllers/prediction_controller.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 
 predictionModel = pickle.load(open('./Models/prediction.pkl', 'rb'))
+
 def predict():
     _json = request.json    
     try:
@@ -12,11 +13,12 @@ def predict():
         # Make a prediction
         prediction = predictionModel.predict(query_df)  # Model expects a 2D array
         price = float(prediction[0])  # Assuming the model returns a 1D array, get the first value as float
-        print("Prediction result:", price)  # Debug print statement
+        print("Prediction result: " ,price) 
         
         # Return the prediction as a JSON response
-        return jsonify({'prediction price: ': price})
+        return jsonify(query_df.to_json(orient='records')), 200
     
     except Exception as e:
+        e = list(e) if isinstance(e, set) else e
         print("Error during prediction:", str(e))  # Debug print statement
         return jsonify({'error': str(e)}), 500
