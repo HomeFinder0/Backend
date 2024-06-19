9e79282f393c7038263701c13bf8fe157244bc2d
@Rewan-Adel <rewanadel1266@gmail.com>
2024-06-19 22:45:09 +0300

    Adding the endpoints into route

--- Modules/residence/routes/residence.route.js
+++ Modules/residence/routes/residence.route.js
@@ -25,6 +25,17 @@ const {
     filtration,
     recommend,
     predictPrice,
+
+    getSalePrice,
+    updateSalePrice,
+
+    acceptBooking,
+    getPurchasedResidences,
+
+    book,
+    getBookedBy,
+    cancelBooking
+
 } = require("../controllers/residence.controller.js");
 const authMiddleware = require("../../authentication/middlewares/auth.middleware.js");
 const { uploadMultiple } = require("../../../Utils/multer.js");
@@ -47,13 +58,18 @@ router.get("/nearest?", getNearestResidences);
 router.get("/all?", getAllApproved);
 router.get("/images/:residenceId", getResidenceImages);
 router.get("/location/:residenceId", getLocation);
+router.get("/price/:residenceId", getSalePrice);
+router.get("/all/purchase?", getPurchasedResidences);
 router.get("/filter?", filtration);
+
+
 
 router.patch("/update/:residenceId", updateResidence);
 router.patch("/update/1st/:residenceId", stepOneUpdate);
 router.patch("/update/2nd/:residenceId", stepTwoUpdate);
 router.patch("/update/3rd/:residenceId", stepThreeUpdate);
 router.patch("/update/4th/:residenceId", stepFourUpdate);
+router.patch("/price/:residenceId", updateSalePrice);
 
 router.delete("/delete/:residenceId", deleteOneResidence);
 router.delete("/image/:imageId", deleteResidenceImage);
@@ -62,5 +78,9 @@ router.delete("/image/:imageId", deleteResidenceImage);
 router.get('/predict/:residenceId', predictPrice);
 router.get('/recommend/:residenceId', recommend);
 
+router.get("/accept-book/:residenceId/:userId", acceptBooking);
+router.get("/cancel-book/:residenceId/:userId", cancelBooking);
+router.get("/make-book/:residenceId",  book);
+router.get("/users-booked/:residenceId", getBookedBy);
 
 module.exports = router;
